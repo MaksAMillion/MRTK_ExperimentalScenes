204a06a08a5fc9be225e061e49f63ebf30b15085
Maximiliano Barragan <maxx852@yahoo.com>
2017-12-19 09:02:15 -0800

    Add an intro frame.

---
+++ Assets/Scripts/MoveAlong.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class MoveAlong : MonoBehaviour, IInputClickHandler {
+    private InputManager inputManager;
+
+    private void Start()
+    {
+        inputManager = InputManager.Instance;
+
+        if (inputManager != null)
+        {
+            inputManager.OverrideFocusedObject = gameObject;
+        }
+
+        
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (inputManager != null)
+        {
+            gameObject.transform.Rotate(0, 10, 0);
+            inputManager.OverrideFocusedObject = null;
+        }
+        
+    }
+}
